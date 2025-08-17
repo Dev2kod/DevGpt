e53abb115e267e7eab683fc148aaebfa6bbdf442
dev2kod <devesh4coding@gmail.com>
2025-08-17 20:02:30 +0530

    done with homepage animation and designing, initializing the chat section

--- frontend/src/routes/homepage/Homepage.jsx
+++ frontend/src/routes/homepage/Homepage.jsx
@@ -2,22 +2,30 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./homepage.css";
 
+
 const Homepage = () => {
   const navigate = useNavigate();
   return (
     <>
       <div className="home">
+        <img src="./orbital.png" alt="" className="orbital" />
         <div className="left">
-          <h1>Dev-GPT</h1>
+          <h1>DEVGPT</h1>
           <h2>One of your closest friends</h2>
-          <h3>
+           <h3>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
             nobis adipisci recusandae.
           </h3>
+          <button onClick={()=>navigate('/dashboard')}>Get Started</button>
         </div>
 
         <div className="right">
-          <h1>Right</h1>
+          <div className="imgcontainer">
+            <div className="bgcontainer">
+              <div className="bg"></div>
+            </div>
+            <img src="./bot.png" alt="" className="bot"/>
+          </div>
         </div>
       </div>
     </>
--- frontend/src/routes/signin/Signin.jsx
+++ frontend/src/routes/signin/Signin.jsx
@@ -4,7 +4,7 @@ import "./signin.css"
 const Signin = () => {
   return (
     <div className="signin">
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up"/>
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up"  forceRedirectUrl="/dashboard"/>
     </div>
   );
 };
