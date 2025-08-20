b0b37c6a7e34265e3bf84f5538591cf5ba8a0b84
dev2kod <devesh4coding@gmail.com>
2025-08-20 14:43:07 +0530

    stabilized ui with css upto chat section

--- frontend/src/routes/chatpage/Chatpage.jsx
+++ frontend/src/routes/chatpage/Chatpage.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { useEffect, useRef } from "react"
+import "./chatpage.css"
+
 
 const Chatpage = () => {
+  const endref = useRef(null);
+
+  useEffect(() => {
+    endref.current.scrollIntoView({behaviour: "smooth"})
+  
+  }, [])
+  
   return (
-    <div>Chatpage</div>
+
+    <div className="chatpage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="messageUser">Test Message from user</div>
+          <div className="messageAi">Test Message from ai</div>
+          <div className="messageUser">Test Message from user</div>
+          <div className="messageAi">Test Message from ai</div>
+          <div className="messageUser">Test Message from user</div>
+          <div className="messageAi">Test Message from ai</div>
+          <div className="messageUser">Test Message from user</div>
+          <div className="messageAi">Test Message from ai</div>
+          <div className="messageUser">Test Message from user</div>
+          <div className="messageAi">Test Message from ai</div>
+          <div className="messageUser">Test Message from user</div>
+          <div className="messageAi">Test Message from ai</div>
+          <div className="messageUser">Test Message from user</div>
+          <div className="messageAi">Test Message from ai</div>
+          <div className="messageUser">Test Message from user</div>
+          <div className="messageAi">Test Message from ai</div>
+          <div className="messageUser">Test Message from user</div>
+          <div className="messageAi">Test Message from ai</div>
+          <div ref={endref}></div>
+        </div>
+      </div>
+
+      
+    </div>
   )
 }
 
--- frontend/src/routes/dashboard/Dashboard.jsx
+++ frontend/src/routes/dashboard/Dashboard.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import "./dashboard.css";
+import {useNavigate} from "react-router-dom"
 
-const dashboard = () => {
+const Dashboard = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className="dashboard">
-      <div className=""></div>
-      <div className=""></div>
+      <div className="texts">
+        <div className="logo">
+          <img src="/logo.png" alt="logo" />
+          <h1>DEVGPT</h1>
+        </div>
+
+        <div className="options">
+          <div className="option">
+            <img src="/chat.png" alt="chat" />
+            <span>Create New Chat</span>
+          </div>
+          <div className="option">
+            <img src="/chat.png" alt="chat" />
+            <span>Create New Chat</span>
+          </div>
+          <div className="option">
+            <img src="/chat.png" alt="chat" />
+            <span>Create New Chat</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="formContainer">
+        <input
+          type="text"
+          placeholder="Ask Me Anything..."
+          className="input"
+        />
+        <button className="button" onClick={()=>navigate("/dashboard/chats/124")}>
+          <img src="/arrow.png" alt="arrow" className="arrow" />
+        </button>
+      </div>
     </div>
   );
 };
 
-export default dashboard;
+export default Dashboard;
